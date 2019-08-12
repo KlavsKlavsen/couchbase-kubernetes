ad0b48a6057abb60ad0b44853cab9cca5dfef1bf
Klavs Klavsen <klavs@enableit.dk>
2019-08-12 13:53:21 +0200

    reworked to use all community editions and upstream etcd - deployed by f.ex. etcd-operator or whatever.

---
+++ scripts/deploy-couchbase.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Deploys/redeploys couchbase
+# - Requires there to be a ETCD cluster running.
+#
+# params: $1 = namespace
+#         $2 = pod name
+#
+# NB. $2 is ALSO used to calculate etcd cluster servicename !
+#
+
+set -ue
+
+for f in couchbase-admin-server.yaml couchbase-server.yaml
+do
+  TMP=$(mktemp)
+  cat ./replication-controllers/$f | sed -e "s:NAMESPACE:$1:" | sed -e "s:PODNAME:$2:" >$TMP
+  kubectl delete -n $1 -f $TMP
+  kubectl apply -n $1 -f $TMP
+done
